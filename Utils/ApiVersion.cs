af02e7bc39a2b2d1d6a5d8a3bb21dbc1856315ea
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-08-26 01:58:31 -0500

    [ADD] ApiVersion

---
+++ Utils/ApiVersion.cs
@@ -0,0 +1,7 @@
+﻿namespace PotakusAPI.Utils;
+
+public static class ApiVersion
+{
+    private readonly static string api = "api/";
+    public static string V1(string prefix) => api + "V1/" + prefix;
+}
