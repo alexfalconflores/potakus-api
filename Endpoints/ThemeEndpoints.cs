3e1a282092d7ce4b5316c549f9837b3563a80920
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-08-26 01:50:33 -0500

    [ADD] Theme endpoints

---
+++ Endpoints/ThemeEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PotakusAPI.Repositories;
+
+namespace PotakusAPI.Endpoints;
+
+public static class ThemeEndpoints
+{
+    public static void MapThemeEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("themes");
+        group.WithTags("Themes");
+
+        group.MapGet("/", async (IThemeRepository themeR) =>
+        {
+            var themes = await themeR.GetAll();
+            return Results.Ok(themes);
+        })
+        .WithName("GetAllThemes");
+    }
+}
