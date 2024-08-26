a9cc0097f13b0536f4ec23d847396976470c5f34
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-08-26 00:38:22 -0500

    [ADD] DbContext services

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PotakusAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar el DbContext con la cadena de conexion
+builder.Services.AddDbContext<PotakuAppContext>(options => 
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("PotakuAppDb"),
+        sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()
+    ));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
