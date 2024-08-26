2fbe64e61a580812f260d7366e191d71c705127d
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-08-26 01:27:13 -0500

    [ADD] ThemeRepository

---
+++ Repositories/IThemeRepository.cs
@@ -0,0 +1,12 @@
+﻿using PotakusAPI.Models;
+
+namespace PotakusAPI.Repositories;
+
+public interface IThemeRepository
+{
+    Task<IEnumerable<Theme>> GetAll();
+    Task<Theme> GetById(int id);
+    Task<int> Add(Theme theme);
+    Task<int> Update(Theme updateTheme);
+    Task<int> DeleteById(int id);
+}
---
+++ Repositories/ThemeRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PotakusAPI.Data;
+using PotakusAPI.Models;
+
+namespace PotakusAPI.Repositories;
+
+public class ThemeRepository(PotakuAppContext context) : IThemeRepository
+{
+    public async Task<int> Add(Theme theme)
+    {
+        if (theme is null) return 0;
+        context.Themes.Add(theme);
+        return await context.SaveChangesAsync();
+    }
+
+    public async Task<int> DeleteById(int id)
+    {
+        var theme = await context.Themes.FirstOrDefaultAsync(x => x.Id == id);
+        if (theme is null) return 0;
+        context.Themes.Remove(theme);
+        return await context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Theme>> GetAll()
+    {
+        return await context.Themes.ToListAsync();
+    }
+
+    public async Task<Theme> GetById(int id)
+    {
+        return await context.Themes.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<int> Update(Theme updateTheme)
+    {
+        var theme = await context.Themes.FirstOrDefaultAsync(x => x.Id == updateTheme.Id);
+        if (theme is null) return 0;
+        theme.Name = updateTheme.Name;
+        return await context.SaveChangesAsync();
+    }
+}
