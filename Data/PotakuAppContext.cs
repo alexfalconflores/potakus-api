f34e4ef12556b37ad3329e71bd59f08d088b7d4b
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-08-26 00:37:30 -0500

    [ADD] Data Context

---
+++ Data/PotakuAppContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PotakusAPI.Models;
+
+namespace PotakusAPI.Data;
+
+public class PotakuAppContext(DbContextOptions<PotakuAppContext> options) : DbContext(options)
+{
+    public DbSet<Item> Items { get; set; }
+    public DbSet<ItemHistory> ItemHistories { get; set; }
+    public DbSet<List> Lists { get; set; }
+    public DbSet<ListItem> ListItems { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<RolePermission> RolePermissions { get; set; }
+    public DbSet<Theme> Themes { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserLogin> UserLogins { get; set; }
+    public DbSet<UserLoginAttempt> UserLoginAttempts { get; set; }
+    public DbSet<UserSession> UserSessions { get; set; }
+}
