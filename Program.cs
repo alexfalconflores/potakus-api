6ed02bf5f8e5832a596e64c2369619e3cc2c2866
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2024-08-26 01:59:23 -0500

    [ADD] Swagger

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Routing.Patterns;
 using Microsoft.EntityFrameworkCore;
 using PotakusAPI.Data;
+using PotakusAPI.Endpoints;
 using PotakusAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Configurar el DbContext con la cadena de conexion
 builder.Services.AddDbContext<PotakuAppContext>(options => 
@@ -13,8 +18,17 @@ builder.Services.AddDbContext<PotakuAppContext>(options =>
 
 // Registrar los repositorios
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
+builder.Services.AddScoped<IThemeRepository, ThemeRepository>();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Registrar los endpoints
+app.MapThemeEndpoints();
 
 app.Run();
